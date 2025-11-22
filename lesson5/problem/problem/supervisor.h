7afb0435bc381e22d9c4e0d3eeaf3b7999d3f2f3
Maksim D <dbimzer93@gmail.com>
2025-11-22 16:28:51 +0300

    добавлено решение к 5 уроку

---
+++ lesson5/problem/problem/supervisor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "people.h"
+
+/*
+    Надзиратель за уровнем удовлетворённости.
+    Способен наблюдать за состоянием человека
+    Если уровень удовлетворённости человека опустится ниже минимального уровня, Надзиратель
+    побуждает человека танцевать до тех пор, пока уровень уровень удовлетворённости
+    не станет больше или равен максимальному значению
+*/
+class SatisfactionSupervisor : public PersonObserver {
+public:
+    // Конструктор принимает значение нижнего и верхнего уровня удовлетворённости
+    SatisfactionSupervisor(int min_satisfaction, int max_satisfaction) 
+    :min_satisfaction_(min_satisfaction)
+    ,max_satisfaction_(max_satisfaction){
+    }
+
+    void OnSatisfactionChanged(Person& person, int old_value, int new_value) override {
+        if (old_value > new_value && person.GetSatisfaction() < min_satisfaction_) {
+            while (person.GetSatisfaction() < max_satisfaction_) {
+                person.Dance();
+            }
+        }
+    }
+
+    // Напишите реализацию самостоятельно
+private:
+    Person* person = nullptr;
+    int min_satisfaction_;
+    int max_satisfaction_;
+};

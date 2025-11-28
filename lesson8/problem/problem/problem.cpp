0bdeebdf37d329564a2d6dc6ebc59d3eead2f1d5
Maksim D <dbimzer93@gmail.com>
2025-11-28 07:30:45 +0300

    добавление решения задания к 8 уроку

---
+++ lesson8/problem/problem/problem.cpp
@@ -0,0 +1,158 @@
+﻿#define _USE_MATH_DEFINES
+#include "svg.h"
+
+#include <cmath>
+
+
+using namespace std::literals;
+using namespace svg;
+
+namespace {
+
+    Polyline CreateStar(Point center, double outer_rad, double inner_rad, int num_rays) {
+        Polyline polyline;
+        for (int i = 0; i <= num_rays; ++i) {
+            double angle = 2 * M_PI * (i % num_rays) / num_rays;
+            polyline.AddPoint({ center.x + outer_rad * sin(angle), center.y - outer_rad * cos(angle) });
+            if (i == num_rays) {
+                break;
+            }
+            angle += M_PI / num_rays;
+            polyline.AddPoint({ center.x + inner_rad * sin(angle), center.y - inner_rad * cos(angle) });
+        }
+
+        polyline.SetFillColor("red"s);
+        polyline.SetStrokeColor("black"s);
+        return polyline;
+    }
+
+    /*void DrawGreeting() {
+        Document doc;
+        doc.Add(Circle().SetCenter({ 20, 20 }).SetRadius(10));
+        doc.Add(Text()
+            .SetFontFamily("Verdana"s)
+            .SetPosition({ 35, 20 })
+            .SetOffset({ 0, 6 })
+            .SetFontSize(12)
+            .SetFontWeight("bold"s)
+            .SetData("Hello C++"s));
+        doc.Add(CreateStar({ 20, 50 }, 10, 5, 5));
+        doc.Render(std::cout);
+    }*/
+
+}  // namespace
+
+namespace shapes {
+
+    class Triangle : public svg::Drawable {
+    public:
+        Triangle(svg::Point p1, svg::Point p2, svg::Point p3)
+            : p1_(p1)
+            , p2_(p2)
+            , p3_(p3) {
+        }
+
+        // Реализует метод Draw интерфейса svg::Drawable
+        void Draw(svg::ObjectContainer& container) const override {
+            container.Add(svg::Polyline().AddPoint(p1_).AddPoint(p2_).AddPoint(p3_).AddPoint(p1_));
+        }
+
+    private:
+        svg::Point p1_, p2_, p3_;
+    };
+
+    class Star : public svg::Drawable {
+    public:
+        Star(svg::Point center, double outer_rad, double inner_rad, int num_rays)
+            :center_(center)
+            , outer_rad_(outer_rad)
+            , inner_rad_(inner_rad)
+            , num_rays_(num_rays) {
+        }
+
+        void Draw(svg::ObjectContainer& container) const override {
+            container.Add(CreateStar(center_, outer_rad_, inner_rad_, num_rays_));
+        }
+
+    private:
+        svg::Point center_;
+        double outer_rad_;
+        double inner_rad_;
+        int num_rays_;
+    };
+    class Snowman : public svg::Drawable {
+    public:
+        Snowman(svg::Point center, double rad)
+            :center_(center)
+            , rad_(rad) {
+        }
+
+        void Draw(svg::ObjectContainer& container) const override {
+            container.Add(svg::Circle()
+                .SetCenter({ center_.x, center_.y + (5 * rad_) })
+                .SetRadius(rad_ * 2)
+                .SetFillColor("rgb(240,240,240)"s)
+                .SetStrokeColor("black"s));
+            container.Add(svg::Circle()
+                .SetCenter({ center_.x, center_.y + (2 * rad_) })
+                .SetRadius(rad_ * 1.5)
+                .SetFillColor("rgb(240,240,240)"s)
+                .SetStrokeColor("black"s));
+            container.Add(svg::Circle()
+                .SetCenter(center_)
+                .SetRadius(rad_)
+                .SetFillColor("rgb(240,240,240)"s)
+                .SetStrokeColor("black"s));
+        }
+
+    private:
+        svg::Point center_;
+        double rad_;
+    };
+
+} // namespace shapes 
+
+
+template <typename DrawableIterator>
+void DrawPicture(DrawableIterator begin, DrawableIterator end, svg::ObjectContainer& target) {
+    for (auto it = begin; it != end; ++it) {
+        (*it)->Draw(target);
+    }
+}
+
+template <typename Container>
+void DrawPicture(const Container& container, svg::ObjectContainer& target) {
+    using namespace std;
+    DrawPicture(begin(container), end(container), target);
+}
+
+int main() {
+    using namespace svg;
+    using namespace shapes;
+    using namespace std;
+
+    vector<unique_ptr<svg::Drawable>> picture;
+    picture.emplace_back(make_unique<Triangle>(Point{ 100, 20 }, Point{ 120, 50 }, Point{ 80, 40 }));
+    picture.emplace_back(make_unique<Star>(Point{ 50.0, 20.0 }, 10.0, 4.0, 5));
+    picture.emplace_back(make_unique<Snowman>(Point{ 30, 20 }, 10.0));
+
+    svg::Document doc;
+    DrawPicture(picture, doc);
+
+    const Text base_text = 
+        Text()
+        .SetFontFamily("Verdana"s)
+        .SetFontSize(12)
+        .SetPosition({ 10, 100 })
+        .SetData("Happy New Year!"s);
+    doc.Add(Text{ base_text }
+        .SetStrokeColor("yellow"s)
+        .SetFillColor("yellow"s)
+        .SetStrokeLineJoin(StrokeLineJoin::ROUND)
+        .SetStrokeLineCap(StrokeLineCap::ROUND)
+        .SetStrokeWidth(3));
+    doc.Add(Text{ base_text }.SetFillColor("red"s));
+
+    // Выводим полученный документ в stdout
+    doc.Render(cout);
+}
---
+++ lesson8/problem/problem/svg.h
@@ -0,0 +1,248 @@
+
+#ifndef SVG_H
+#define SVG_H
+
+#pragma once
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <optional>
+
+namespace svg {
+
+    using Color = std::string;
+    inline const Color NoneColor{ "none" };
+
+    enum class StrokeLineCap {
+        BUTT,
+        ROUND,
+        SQUARE,
+    };
+
+    enum class StrokeLineJoin {
+        ARCS,
+        BEVEL,
+        MITER,
+        MITER_CLIP,
+        ROUND,
+    };
+
+    std::ostream& operator<<(std::ostream& out, StrokeLineCap value);
+    std::ostream& operator<<(std::ostream& out, StrokeLineJoin value);
+
+    struct Point {
+        Point() = default;
+        Point(double x, double y)
+            : x(x)
+            , y(y) {
+        }
+        double x = 0;
+        double y = 0;
+    };
+
+    /*
+     * Вспомогательная структура, хранящая контекст для вывода SVG-документа с отступами.
+     * Хранит ссылку на поток вывода, текущее значение и шаг отступа при выводе элемента
+     */
+    struct RenderContext {
+        RenderContext(std::ostream& out)
+            : out(out) {
+        }
+
+        RenderContext(std::ostream& out, int indent_step, int indent = 0)
+            : out(out)
+            , indent_step(indent_step)
+            , indent(indent) {
+        }
+
+        RenderContext Indented() const {
+            return { out, indent_step, indent + indent_step };
+        }
+
+        void RenderIndent() const {
+            for (int i = 0; i < indent; ++i) {
+                out.put(' ');
+            }
+        }
+
+        std::ostream& out;
+        int indent_step = 0;
+        int indent = 0;
+    };
+
+    /*
+     * Абстрактный базовый класс Object служит для унифицированного хранения
+     * конкретных тэгов SVG-документа
+     * Реализует паттерн "Шаблонный метод" для вывода содержимого тэга
+     */
+    class Object {
+    public:
+        void Render(const RenderContext& context) const;
+
+        virtual ~Object() = default;
+
+    private:
+        virtual void RenderObject(const RenderContext& context) const = 0;
+    };
+
+    template <typename Owner>
+    class PathProps {
+    public:
+        Owner& SetFillColor(Color color) {
+            fill_color_ = std::move(color);
+            return AsOwner();
+        }
+        Owner& SetStrokeColor(Color color) {
+            stroke_color_ = std::move(color);
+            return AsOwner();
+        }
+        Owner& SetStrokeWidth(double width) {
+            stroke_width_ = std::move(width);
+            return AsOwner();
+        }
+        Owner& SetStrokeLineCap(StrokeLineCap line_cap) {
+            stroke_linecap_ = std::move(line_cap);
+            return AsOwner();
+        }
+        Owner& SetStrokeLineJoin(StrokeLineJoin line_join) {
+            stroke_linejoin_ = std::move(line_join);
+            return AsOwner();
+        }
+
+    protected:
+        ~PathProps() = default;
+
+        // Метод RenderAttrs выводит в поток общие для всех путей атрибуты fill и stroke
+        void RenderAttrs(std::ostream& out) const {
+            using namespace std::literals;
+
+            if (fill_color_) {
+                out << " fill=\""sv << *fill_color_ << "\""sv;
+            }
+            if (stroke_color_) {
+                out << " stroke=\""sv << *stroke_color_ << "\""sv;
+            }
+        }
+
+    private:
+        Owner& AsOwner() {
+            // static_cast безопасно преобразует *this к Owner&,
+            // если класс Owner — наследник PathProps
+            return static_cast<Owner&>(*this);
+        }
+
+        std::optional<Color> fill_color_;
+        std::optional<Color> stroke_color_;
+        double stroke_width_;
+        StrokeLineCap stroke_linecap_;
+        StrokeLineJoin stroke_linejoin_;
+    };
+
+    /*
+     * Класс Circle моделирует элемент <circle> для отображения круга
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
+     */
+    class Circle : public Object, public PathProps<Circle> {
+    public:
+        Circle& SetCenter(Point center);
+        Circle& SetRadius(double radius);
+
+    private:
+        void RenderObject(const RenderContext& context) const override;
+
+        Point center_;
+        double radius_ = 1.0;
+    };
+
+    /*
+     * Класс Polyline моделирует элемент <polyline> для отображения ломаных линий
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline
+     */
+    class Polyline : public Object, public PathProps<Polyline> {
+    public:
+        // Добавляет очередную вершину к ломаной линии
+        Polyline& AddPoint(Point point);
+
+    private:
+        void RenderObject(const RenderContext& context) const override;
+        std::vector<Point> points_;
+    };
+
+    /*
+     * Класс Text моделирует элемент <text> для отображения текста
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
+     */
+    class Text : public Object, public PathProps<Text> {
+    public:
+        // Задаёт координаты опорной точки (атрибуты x и y)
+        Text& SetPosition(Point pos);
+
+        // Задаёт смещение относительно опорной точки (атрибуты dx, dy)
+        Text& SetOffset(Point offset);
+
+        // Задаёт размеры шрифта (атрибут font-size)
+        Text& SetFontSize(uint32_t size);
+
+        // Задаёт название шрифта (атрибут font-family)
+        Text& SetFontFamily(std::string font_family);
+
+        // Задаёт толщину шрифта (атрибут font-weight)
+        Text& SetFontWeight(std::string font_weight);
+
+        // Задаёт текстовое содержимое объекта (отображается внутри тэга text)
+        Text& SetData(std::string data);
+
+    private:
+        void RenderObject(const RenderContext& context) const override;
+        Point position_;
+        Point offset_;
+        uint32_t font_size_ = 1;
+        std::string font_family_;
+        std::string font_weight_;
+        std::string data_;
+    };
+
+    class ObjectContainer {
+    public:
+        template <typename ObjectType>
+        void Add(ObjectType object) {
+            AddPtr(std::make_unique<ObjectType>(std::move(object)));
+        }
+
+        virtual void AddPtr(std::unique_ptr<Object>&& obj) = 0;
+    };
+
+    class Drawable {
+    public:
+        virtual ~Drawable() = default;
+        virtual void Draw(ObjectContainer& container) const = 0;
+    };
+
+    class Document : public ObjectContainer {
+    public:
+        /*
+         Метод Add добавляет в svg-документ любой объект-наследник svg::Object.
+         Пример использования:
+         Document doc;
+         doc.Add(Circle().SetCenter({20, 30}).SetRadius(15));
+        */
+        template <typename ObjectType>
+        void Add(ObjectType object) {
+            AddPtr(std::make_unique<ObjectType>(std::move(object)));
+        }
+
+        // Добавляет в svg-документ объект-наследник svg::Object
+        void AddPtr(std::unique_ptr<Object>&& obj) override;
+
+        // Выводит в ostream svg-представление документа
+        void Render(std::ostream& out) const;
+
+    private:
+        std::vector<std::unique_ptr<Object>> objects_;
+    };
+
+}  // namespace svg
+
+#endif // SVG_H
